d5f9d290dee3bf33a47907155f625bbccfdfd984
Aarron10 <aarronaarron25@gmail.com>
2025-02-09 02:42:59 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,27 @@
+const messages = [
+    "Are you sure ?",
+    "Cent percent??",
+    "Mango shake vangi theram?",
+    "Pookie please...",
+    "Just think about it!",
+    "If you say no, njan karayum...",
+    "If u say no, njan sherikum karayume...",
+    "Njan karayuva...",
+    "Ok fine, I will stop asking...",
+    "Just kidding, say yes please! ❤️"
+];
+
+let messageIndex = 0;
+
+function handleNoClick() {
+    const noButton = document.querySelector('.no-button');
+    const yesButton = document.querySelector('.yes-button');
+    noButton.textContent = messages[messageIndex];
+    messageIndex = (messageIndex + 1) % messages.length;
+    const currentSize = parseFloat(window.getComputedStyle(yesButton).fontSize);
+    yesButton.style.fontSize = `${currentSize * 1.5}px`;
+}
+
+function handleYesClick() {
+    window.location.href = "yes_page.html";
+}
